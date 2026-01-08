a1e1c8406d0b3b30d4abd1d126be2901398b63e7
olveraedik41-ui <olveraedik41@gmail.com>
2026-01-08 11:24:01 -0600

    Create Erick.py

---
+++ Erick.py
@@ -0,0 +1,34 @@
+print("Nombre completo:")
+print("Erick Ahron Olvera Cruz")
+
+print("Fortalezas:")
+print("- Puntual")
+print("- Creativo")
+print("- Perseverante")
+
+print("Debilidades:")
+print("- A veces me distraigo fácilmente me pongo nervioso al hablar con personas que no conozco")
+
+print("Gustos:")
+print("- Escuchar música")
+print("- Ver series")
+print("- Pasar tiempo con mi familia")
+print("- Aprender cosas nuevas en el mundo del deporte")
+
+print("Canciones favoritas:")
+print("- 'Soy de rancho ")
+print("- 'Lokota'")
+
+print("Artistas favoritos:")
+print("- El komander")
+print("-Peso pluma")
+print("-Fuerza Regida")
+
+print("Resumen de mis vacaciones:")
+print("Durante mis vacaciones descansé en casa, pasé tiempo con mi familia,")
+print("salí con amigos, escuché música y aproveché para relajarme.")
+print("También realicé algunas actividades deportivas.")
+print()
+
+print("Estado de ánimo actual:")
+print("Motivado")
