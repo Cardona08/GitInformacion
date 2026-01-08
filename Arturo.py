4de4e88930c6206269f5c0889e9ceefafcfc7d5d
Cardona08 <lparturocardona@gmail.com>
2026-01-08 08:38:55 -0600

    Ps ya Cabe mi PY

---
+++ Arturo.py
@@ -0,0 +1,16 @@
+print("=============================================================================================")
+print("                               Arturo Cardona Mendez                                         ")
+print("=============================================================================================")
+print("                Que soy Optimista , Creativo , Empatico y Honesto                            ")
+print("=============================================================================================")
+print("             Me debilidad es a los Petatiados y a los muñecos budhu Aca                      ")
+print("=============================================================================================")
+print("            Mi cancion Favorita y artista es: Sabanas Blancas  , Bruno Mars                  ")
+print("=============================================================================================")
+print("Ps todas las Vacaiones en si me la Pase Enfermo entoces Navidad y Año Nuevo Estuve enfermo en " \
+"la casa y ps mi familia me abandono para ir a con la familia festejar pero estuvo bien por que vi " \
+"muchas series")
+print("=============================================================================================")
+print("Y mi Estado Emocional es Alegre por Volver a estar en la Escula y ya no Estar aplastado en la" \
+"casa y no ser sobre explotado en mi trabajo de Nuevo")
+print("=============================================================================================")
